7c0b653302cfb412560aeb5d8923e8505d788153
Cl0udKiD <sasha.azarov8@gmail.com>
2023-01-17 14:45:14 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
-}
+  var arr1=[];
+        for (let i in matrix){
+            if (i%2==1){arr1.push(matrix[i].reverse())}else{arr1.push(matrix[i])}
+        }
+        var arr2=[]
+        for (i in arr1){
+          for (j in arr1[i]){arr2.push(arr1[i][j])}
+        }
+        return arr2
+  }
